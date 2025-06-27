d88ca242c23c8392a1a16712c86f4f5fc76ec4ed
MeghnaTomar17 <66015551+MeghnaTomar17@users.noreply.github.com>
2025-06-27 12:54:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const mandiData = require('./mandi-data.json');
+const mandiData = require('.mandi-data.json');
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Mandi API is running!");
+  res.send("Welcome to the KrishiNect Mandi API!");
+   res.json([
+    { crop: "Wheat", price: "₹2200/quintal", mandi: "Meerut" },
+    { crop: "Rice", price: "₹1800/quintal", mandi: "Patna" }
+  ]);
 });
+
 
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
